58b9a7a9788297ddc5bb279df4e5c36595dfccd9
Magdalena Houska <magdalena.houska@gmail.com>
2019-11-10 14:49:11 +0100

    Creating a recipe view.

--- client/src/Main.elm
+++ client/src/Main.elm
@@ -1,7 +1,7 @@
 module Main exposing (..)
 
 import Browser 
-import Html exposing (Html, div, text)
+import Html exposing (Html, div, text, ul, li)
 
 main =
     Browser.sandbox { init = 0, update = update, view = view }
@@ -13,7 +13,43 @@ update msg model =
     case msg of
         Increment -> 1
 
+toHtmlMsg : String -> (Html li)
+toHtmlMsg element = 
+    li [] ( [ text element ] )
+
+type Method = Method String
+type Serves = Int
+type Time = Time String
+type Ingredients = List ( String )
+type Recipe = {
+    
+}
+
 
 view model = 
+    let 
+        ingredients = 
+            ["20 g flour",
+            "50 ml milk"]
+        
+        method = "Do this. Then do that."
+
+        serves = 6
+
+        time = "1h 10min"
+
+        ingredientsAsHtml : List ( Html Msg ) 
+        ingredientsAsHtml = 
+            List.map toHtmlMsg ( ingredients )
+    in
     div []
-        [ text "Hello world" ]
+        [ div [] [ text "Ingredients:" ]
+        , ul [] ingredientsAsHtml
+        , div [] [ text "Method: "]
+        , div [] [ text method ]
+        , div [] [ text ( "Serves: " ++ String.fromInt serves ) ]
+        , div [] [ text ( "Time to do it: " ++ time )]
+        ]
+        
+    
+    
